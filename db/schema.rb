a29b7b0ca595453e6b58a44ab1fa962194e4bd25
hiroooootaro <taro.hirotaro@icloud.com>
2020-09-28 18:36:36 +0900

    templateテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_23_054532) do
+ActiveRecord::Schema.define(version: 2020_09_28_091651) do
+
+  create_table "templates", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "view_file", null: false
+    t.string "view_name", null: false
+    t.integer "view_type", null: false
+    t.integer "category", null: false
+    t.string "image", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
