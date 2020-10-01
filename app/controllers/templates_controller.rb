7cb8371dcb4f921b70b139b148984030862cfe13
hiroooootaro <taro.hirotaro@icloud.com>
2020-10-01 12:36:32 +0900

    コントローラーの設定とビューファイルのカラム名設定。

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -1,9 +1,19 @@
 class TemplatesController < ApplicationController
+
   def index
-    
   end
 
   def new
-    
   end
+
+  def create
+    @template = Template.new(template_params)
+  end
+
+  private
+
+  def template_params
+    params.require(:template).permit(:template_explain, :category, :template_image ).merge(user_id: current_user.id)
+  end
+
 end
